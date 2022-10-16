e80697a4ea9d1b5d8092505124c82bb6983ae0d1
Sayū <42917989+Abytsu@users.noreply.github.com>
2022-10-16 13:06:04 -0300

    Delete Beecrowd directory

--- Beecrowd/History_Exercise.py
+++
@@ -1,9 +0,0 @@
-while True:
-    entrada = int(input())
-    ativacao,x = 0,0
-    while ativacao != 1:
-        x += 1 
-        if entrada <= (x*100) and entrada >= ((x*100)-99):
-            century = x
-            ativacao = 1
-    print(century)
